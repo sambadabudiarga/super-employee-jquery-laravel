0b59b71ab6fe6df8a15c63736fa5d509cc166c7c
Sambada Budiarga <sambada.working@gmail.com>
2018-11-17 12:57:36 +0800

    create form and js save function

--- public/appjs/employees/index.js
+++ public/appjs/employees/index.js
@@ -1,4 +1,5 @@
 var app_datatable;
+var add_mode = false;
 
 $(function() {
     app_datatable = $("#data_list").DataTable({
@@ -20,4 +21,63 @@ $(function() {
           },
         ]
     });
+});
+
+$('a.btn_add').click(function() {
+    // hide if already add mode
+    if (add_mode) {
+      $('form.form_add').parent('div').addClass('hidden');
+    } else {
+      // otherwise, show it
+      clearInput();
+      $('form.form_add').parent('div').removeClass('hidden');
+    }
+
+    add_mode = !add_mode;
+})
+
+function clearInput() {
+    $('input[name=first_name]').val("");
+    $('input[name=last_name]').val("");
+    $('input[name=age]').val("");
+    $('select[name=country_id]').val("");
+}
+
+$('form.form_add').submit(function() {
+  var form_data = $(this).serialize();
+
+  // remove all error marker and message
+  $(this).find('.form-group.has-error').removeClass('has-error');
+  $(this).find('[data-original-title]').each(function() {
+    this.title = $(this).data('originalTitle');
+  })
+
+  $.post(urlRoot + '/employees', form_data)
+   .done(function(data) {
+    console.log(data);
+    alert('Data saved');
+
+    clearInput();
+
+    // reload datatable
+    app_datatable.ajax.reload();
+   })
+   .fail(function(jqXhr) {
+     console.error(jqXhr);
+
+     if (jqXhr.status == 422) {
+      // set error element
+      Object.keys(jqXhr.responseJSON).forEach(function(elm) {
+        console.log(elm);
+        $('[name="' + elm + '"]').closest('.form-group').addClass('has-error');
+        $('[name="' + elm + '"]').data('originalTitle', $('[name="' + elm + '"]')[0].title);
+        $('[name="' + elm + '"]')[0].title = jqXhr.responseJSON[elm][0];
+      });
+
+      alert('Please check your data!');
+     } else {
+      alert('Error saving data');
+     }
+  });
+  return false;
 });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,8 +8,48 @@
                 <div class="panel-heading">{{ config('app.name') }}</div>
                 
                 <div class="panel-body">
-                    <h1>Hello World!</h1>
                     <div class="row">
+                        <div class="col-sm-12" style="margin-bottom: 10px">
+                            <a class="btn_add btn btn-default"><i class="fa fa-plus"></i> Add</a>
+                        </div>
+                        <div class="col-sm-12 hidden">
+                            {{ Form::open(['class' => 'form_add form-horizontal']) }}
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <h1>Profile picture</h1>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <div class="row">
+                                            <div class="form-group">
+                                                <div class="col-sm-8">
+                                                    {{ Form::text('first_name', null, ['class' => 'form-control']) }}
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="col-sm-8">
+                                                    {{ Form::text('last_name', null, ['class' => 'form-control']) }}
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="col-sm-3">
+                                                    {{ Form::number('age', null, ['class' => 'form-control', 'min' => 17, 'max' => 65, 'step' => 1]) }}
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="col-sm-12">
+                                                    {{ Form::select('country_id', $countries, null, ['class' => 'form-horizontal', 'placeholder' => 'Select country']) }}
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="col-sm-12" style="margin-top: 20px">
+                                                    <button class="btn btn-primary btn_save"><i class="fa fa-user"></i> Save Employee</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            {{ Form::close() }}
+                        </div>
                         <div class="col-sm-12">
                             <table id="data_list" class="table table-striped table-hover">
                                 <thead>
